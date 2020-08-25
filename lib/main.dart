8cb045a3e358f0e953f25f8376d4eb7d212ac465
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-08-25 16:45:08 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/main_model.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,19 +10,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('コリアンダー')
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('コリアンダー')
+          ),
+            body: Consumer<MainModel>(builder: (context, model, child) {
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                      model.tanakaText,
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  RaisedButton(
+                    child: Text('ボタン'),
+                    onPressed: (){
+                      model.changeTanakaText();
+                    },
+                  ),
+                ],
+              ),
+            );
+          }),
         ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Text(
-                'ゆうきさんじゃないよ！'
-              )
-            ],
-          )
-        )
       ),
     );
   }
