3d604bde021ca36fbdca69453bc66fa5b9ddda74
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-08-25 15:43:52 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/next_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'flutter学習のためのアプリ',
-      theme: ThemeData(
-
-        primarySwatch: Colors.green,
-
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('コリアンダー')
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text(
+                'ゆうきさんじゃないよ！'
+              )
+            ],
+          )
+        )
       ),
-      home: MyHomePage(title: 'flutter学習のためのアプリ'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  String text = '次へ';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text('田中ユウキのflutter大学'),
-      ),
-      body: Container(
-        width: double.infinity,
-          child: TextFormField(
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-            decoration: InputDecoration(
-              hintText: '入力してね'
-            ),
-          ),
-          ),
-      );
-  }
-}
